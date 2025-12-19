fcf7099f714cd998c2a6bab3e1db3fa7cf460501
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-12-19 18:55:42 +0530

    feat: extract icon components for better modularity
    - Create XIcon component for X/Twitter icon
    - Create DiscordIcon component for Discord icon
    - Improves code organization and reusability

---
+++ frontend/src/components/assets/XIcon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function XIcon() {
+  return (
+    <svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M18.229 0H21.7557L14.0509 8.89552L23.1152 21H16.0177L10.459 13.6585L4.09841 21H0.56935L8.81049 11.4854L0.115234 0H7.39253L12.4173 6.71021L18.229 0ZM16.9912 18.8676H18.9455L6.33076 2.02046H4.23381L16.9912 18.8676Z"
+        fill="#FBFBFB"
+      />
+    </svg>
+  );
+}
+
+export default XIcon;
